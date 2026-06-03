ec2c41c34edaa7a6c9ab9271e3a54737f17aa3c6
makarim muhammad <jakamakarim@gmail.com>
2026-06-04 01:32:18 +0700

    feat: add theGood and theBad fields to BookReview and MovieReview models

---
+++ prisma/migrations/20260603175659_noir_update/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "BookReview" ADD COLUMN "theBad" TEXT;
+ALTER TABLE "BookReview" ADD COLUMN "theGood" TEXT;
+
+-- AlterTable
+ALTER TABLE "MovieReview" ADD COLUMN "theBad" TEXT;
+ALTER TABLE "MovieReview" ADD COLUMN "theGood" TEXT;
