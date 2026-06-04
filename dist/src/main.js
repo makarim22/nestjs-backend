e577bf9d5cde4fd9ae463cb83175ff2c7acf83d8
makarim muhammad <jakamakarim@gmail.com>
2026-06-04 18:06:04 +0700

    feat: configure static asset serving for uploads directory and enable CORS in bootstrap

--- dist/src/main.js
+++ dist/src/main.js
@@ -9,7 +9,9 @@ async function bootstrap() {
     app.useStaticAssets((0, path_1.join)(process.cwd(), 'uploads'), {
         prefix: '/uploads/',
     });
-    await app.listen(process.env.PORT ?? 3000);
+    const port = process.env.PORT ?? 8080;
+    await app.listen(port, '0.0.0.0');
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
   
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 8080;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 // trigger restart
