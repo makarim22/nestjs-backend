ebfb167bb98a316d3cdc6ea8303146035c1834ff
makarim muhammad <jakamakarim@gmail.com>
2026-06-08 18:08:12 +0700

    chore: remove unused scaffolded test files and one-off seed/fix scripts

--- update_status_sql.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./dev.db');
-db.serialize(() => {
-  db.run("UPDATE MovieReview SET status = 'APPROVED'");
-  db.run("UPDATE BookReview SET status = 'APPROVED'");
-});
-db.close(() => console.log('Updated!'));
